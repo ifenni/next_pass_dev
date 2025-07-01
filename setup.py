fcb30e0b430c777fee9c4464df2fc7802ec32dab
cmspeed <cole.speed@jpl.nasa.gov>
2025-07-01 16:51:31 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="next_pass",
+    version="0.1.0",
+    py_modules=["next_pass", "collection_builder", "landsat_pass", "sentinel_pass", "opera_products", "plot_maps", "utils"],
+    install_requires=[],
+    description="Tool to find upcoming satellite overpasses and OPERA product availability",
+    author="OPERA-Cal-Val",
+    url="https://github.com/cmspeed/next_pass",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
