6265e8904cd48ca61010ae4b14166bcf75eca0ca
cmspeed <cole.speed@jpl.nasa.gov>
2025-10-06 12:58:51 -0700

    Update opera_products.py to differentiate RTC VV/VH and CSLC-VV

--- opera_products.py
+++ opera_products.py
@@ -141,7 +141,7 @@ def export_opera_products(results_dict, timestamp_dir):
              "Download URL WTR", "Download URL BWTR", "Download URL CONF",
              "Download URL VEG-ANOM-MAX", "Download URL VEG-DIST-STATUS",
              "Download URL VEG-DIST-DATE", "Download URL VEG-DIST-CONF",
-             "Download URL VV", "Download URL VH", "Geometry (WKT)"]
+             "Download URL RTC-VV", "Download URL RTC-VH", "Download URL CSLC-VV", "Geometry (WKT)"]
         )
 
         for dataset, data in results_dict.items():
@@ -173,8 +173,9 @@ def export_opera_products(results_dict, timestamp_dir):
                     "veg_dist_status": "N/A",
                     "veg_dist_date": "N/A",
                     "veg_dist_conf": "N/A",
-                    "vv": "N/A",
-                    "vh": "N/A"
+                    "rtc-vv": "N/A",
+                    "rtc-vh": "N/A",
+                    'cslc-vv': "N/A",
                 }
 
                 keyword_map = {
@@ -185,8 +186,9 @@ def export_opera_products(results_dict, timestamp_dir):
                     'VEG-DIST-STATUS': 'veg_dist_status',
                     'VEG-DIST-DATE': 'veg_dist_date',
                     'VEG-DIST-CONF': 'veg_dist_conf',
-                    'VV': 'vv',
-                    'VH': 'vh'
+                    '_30_v1.0_VV': 'rtc-vv',
+                    '_30_v1.0_VH': 'rtc-vh',
+                    '_VV_v1.1': 'cslc-vv',
                 }
                 
                 related_urls = umm.get("RelatedUrls", [])
@@ -209,7 +211,7 @@ def export_opera_products(results_dict, timestamp_dir):
                     urls["water"], urls["bwater"], urls["water_conf"],
                     urls["veg_anom_max"], urls["veg_dist_status"],
                     urls["veg_dist_date"], urls["veg_dist_conf"],
-                    urls["vv"], urls["vh"], geom_wkt
+                    urls["rtc-vv"], urls["rtc-vh"], urls["cslc-vv"], geom_wkt
                 ])
     LOGGER.info(
         "-> OPERA products metadata successfully saved"
